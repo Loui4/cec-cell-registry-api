ad24ff73213da733bccf56b5b0604d20aa929964
Louis Nyirongo <37172974+Loui4@users.noreply.github.com>
2022-01-29 19:46:04 +0200

    routes configured

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,29 @@ import App from '@/app';
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
+import MemberRoute from '@routes/member.route';
+import YearRoute from '@routes/year.route';
+import BibleClassMemberShipRoute from '@routes/bible-class-membership.route';
+import BibleClassRoute from '@routes/bible-class.route';
+import WeekRoute from '@routes/week.route';
+import CellAttendanceRoute from '@routes/cell-attendance.route';
+import ChurchRoute from '@routes/church.route';
+
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
+const app = new App([
+  new IndexRoute(),
+  new UsersRoute(),
+  new AuthRoute(),
+  new MemberRoute(),
+  new BibleClassMemberShipRoute(),
+  new BibleClassRoute(),
+  new WeekRoute(),
+  new YearRoute(),
+  new CellAttendanceRoute(),
+  new ChurchRoute()
+]);
 
 app.listen();
